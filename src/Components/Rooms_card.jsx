c3ae4f755b8979c0aad9d64ab7e608a59d2e1ee5
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-07-03 21:21:39 -0230

    Checkout page made

--- src/Components/Rooms_card.jsx
+++ src/Components/Rooms_card.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
-import "./RoomsCard.css"
+import "./RoomsCard.css";
+import {GiWaterPolo,GiWireframeGlobe,GiZigzagTune} from 'react-icons/gi';
+import {BsWifi} from 'react-icons/bs'
+import { ImStarEmpty } from "react-icons/im";
 function Rooms_card(props) {
   return (
     <div class="Rooms__hotelCarousal__card">
                 <div class='Rooms__hotelCarousal__card__image'>
                     <img src={props.link} height="150" width="280" />
                 </div>
+
+                <div class='Rooms__hotelCarousal__card__image'>
+                    {props.heading}
+                </div>
+                <div class='Rooms__hotelCarousal__card__rating'>
+                <ImStarEmpty onClick={()=>console.log('a')} /> {props.rating}
+                </div> 
+                
                 <div class='Rooms__hotelCarousal__card__utilities'>
+<GiWaterPolo />
+<GiWireframeGlobe />
+<GiZigzagTune />
+<BsWifi />
 
                 </div>
+                <button className="Rooms__hotelCarousal__card__button" onClick={()=>console.log('booked')}>Book Now</button>
     </div>
   )
 }
