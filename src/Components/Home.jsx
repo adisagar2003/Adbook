121321c7208a121fc03a2bdd20782b56a6cfb96c
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-07-03 13:35:38 -0230

    Navbar added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,32 +1,47 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import "./Home.css";
 import hotelLogo from './icons/alex-3.svg';
 import {BiMenu} from 'react-icons/bi'
+import Home_Title from './Home_Title';
 function Home() {
+    const navReference = useRef(null);
+    const buttonReference = useRef(null);
+    const toggleSidebar = ()=>{
+       
+        navReference.current.classList.add('active');
+        console.log(navReference.current);
+
+    }
   return (
+    
     <div className='Home-layout'>
-      <div className='Home-layout__navbar'>
+      <div  className='Home-layout__navbar'>
         <a class='Home-layout__navbar__logo'>
         <img src={hotelLogo} alt="hotel Logo" width="70" height="70" />
 
         </a>
-        <div className='Home-layout__navbar__side'>
+        <div className='Home-layout__navbar__side' >
             <a>Home</a>
             <a>About</a>
             <a>Contact</a>
             <a className='Home-layout__navbar__side__signIn'>Sign in</a>
     
         </div>
-        <div className='Home-layout__hamburger-icon' onClick={()=>console.log('hi')}>
+     
+        <div className='Home-layout__hamburger-icon' onClick={toggleSidebar}>
 <BiMenu />
         </div>
+        
      
 
       </div>
-      <div className='Home-layout__searchHotels'>
-
-<h1></h1>
-</div>
+      
+<div className='Home-layout__sidebar' ref={navReference}>
+<a>Home<button ref={buttonReference} class='Home-layout__sidebar__button' onClick={()=>navReference.current.classList.remove("active")}>X</button></a>
+            <a>About</a>
+            <a>Contact</a>
+        </div>
+<Home_Title />
     </div>
   )
 }
---
+++ src/Components/Home_Title.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './HomeTitle.css'
+function Home_Title() {
+  return (
+    <div className='Home_Title-layout'>
+        
+        
+   <div className='Home_Title-layout__heading'>
+    
+   </div>
+          
+   <div>
+    I say g
+   </div>
+        
+        
+        </div>
+  )
+}
+
+export default Home_Title
