b56d4e65e69f3455d6b13ffcd539c3da92d3ac72
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-07-05 12:00:54 -0230

    JWT cookie deployed

--- server/models/UserSchema.js
+++ server/models/UserSchema.js
@@ -28,7 +28,6 @@ const UserSchema = new mongoose.Schema({
         type:String,
         required:true
     }
-
 });
 const UserModel = mongoose.model("User",UserSchema);
 module.exports = UserModel
--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ const server = express();
 const HotelModel = require("./models/HotelSchema")
 server.use(express.json());
 const UserModel = require('./models/UserSchema');
-
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+server.use(cookieParser());
 dotenv.config();
 mongoose.connect(MONGO="mongodb+srv://adi:sahara123@cluster0.drtgg.mongodb.net/hotel-booking-app?retryWrites=true&w=majority").then(()=>{
     console.log('mongoDB database connected')
@@ -39,8 +41,15 @@ server.listen(process.env.PORT||5000,()=>{
 server.post("/login", async (req,res)=>{
    const User =await UserModel.findOne({username:req.body.username,password:req.body.password})
    if (User){
-    console.log(User)
-    res.send("user found")
+    console.log(User);
+    const token = jwt.sign({id:User._id,username:User.username,email:User.email},process.env.JWT);
+    
+    console.log(token
+        )
+       
+    res.cookie("access_token",token,{
+        httpOnly:true,
+    }).send("user found")
 
    }
    else{
