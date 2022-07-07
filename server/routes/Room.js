8856f441ae3549b00c7a104f50bccdd14fcaafc5
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-07-07 18:21:36 -0230

    finding by id mechanism

--- server/routes/Room.js
+++ server/routes/Room.js
@@ -1,7 +1,51 @@
 const express=require('express');
+const HotelModel = require('../models/HotelSchema');
+const UserModel = require('../models/UserSchema');
 const router =express.Router();
 router.get('/details',(req,res)=>{
     res.json({
         room:'room_details'
     })})
+
+router.post('/post',(req,res)=>{
+    HotelModel.create(req.body).then((pr)=>{
+res.json({
+    hotel:"created"
+})
+    }).catch(()=>{
+        res.json({
+            hotel:"not_created"
+        })
+    })
+})
+//getting hotels by id;
+router.get('/find/:_id', async (req,res)=>{
+    console.log(req.params['_id']);
+    HotelModel.find({_id:req.params['_id']}).then((hotels)=>{
+console.log('hotels',hotels);
+res.json({
+    hotels
+})
+
+  }).catch(
+console.log('err')
+  )
+
+})
+
+//getting hotels by location
+
+router.get('/city/:city', async (req,res)=>{
+    console.log(req.params['city']);
+    HotelModel.find({city:req.params['city']}).then((hotels)=>{
+console.log('hotels',hotels);
+res.json({
+    hotels
+})
+
+  }).catch(
+console.log('err')
+  )
+
+})
 module.exports = router
