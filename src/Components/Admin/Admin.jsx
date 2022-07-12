2df2dc48ec2da25fde5f0cca47eac49b3d8b7943
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-07-12 13:32:18 -0230

    File initialized

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,21 +1,48 @@
+import axios from 'axios';
 import React,{useState} from 'react'
 
 function Admin() {
    const [upload,setUpload] = useState(false);
+   const [fileAlert,setFileAlert] = useState(true);
+   const [ success,setSuccess] = useState(false);
    const [newHotel,setHotel] = useState({
       name:"",
       price:0,
       photo:'',
    })
+   const [name,setName] = useState(null)
    const handlePhoto  = (e)=>{setHotel({
-      ...newHotel,photo:e.target.files[0]
+      ...newHotel,photo:e.target.file
   
    })
    console.log(newHotel)
 }
+const submitHandle = (e)=>{
+e.preventDefault();
+   if (name!=null){
+      
+      axios.post('https://adbook-server.herokuapp.com/admin/upload',{
+         file:newHotel.photo,
+         name:Date.now()+'--'+name
+      }).then((res)=>{
+console.log(res,'this should be the response')
+setSuccess(true)
+      }).catch((error)=>{
+         console.log(error)
+      })
+      setUpload(true)
+      setInterval(()=>{
+         setUpload(false)
+      },4000)
+   }
+   else{
+
+   }
+}
   return (
     <div>
-    <form>
+      {success?<h1>File uploaded!!!!</h1>:<a></a>}
+    <form onSubmit={submitHandle}>
       <main class="flex min-h-screen items-center justify-center bg-gray-100 font-sans">
   <label for="dropzone-file" class="mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center rounded-xl border-2 border-dashed border-blue-400 bg-white p-6 text-center" >Upload file</label>
     <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -26,6 +53,8 @@ function Admin() {
 
 
     <input id="dropzone-file" type="file" class="hidden" onChange={handlePhoto} />
+    <input type="text" onChange={(e)=>setName(e.target.value)} />
+    <button  onSubmit={submitHandle} >Submit</button>
 
 </main>
 
